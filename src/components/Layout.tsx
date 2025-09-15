bb7b150603f4b307ff1fa12fd33d23feccbb32c0
carvalhooosmkt <carvalhooosmkt@gmail.com>
2025-09-15 06:19:43 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,6 @@ import SacredParticles from './SacredParticles';
 export default function Layout() {
   return (
     <div className="min-h-screen bg-obsidian-gradient relative">
-
-       {/* Language Selector */}
-      <div className="absolute top-4 right-4 z-50">
-        <LanguageSelector />
-      </div>
-      
       <SacredParticles />
       <main className="pb-20 relative z-10">
         <Outlet />
